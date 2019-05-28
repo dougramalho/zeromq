48a561813d9e5d276c46bf72bd22272bd8627eaf
Douglas José Ramalho Araujo <douglas.ramalho@squadra.com.br>
2019-05-28 10:16:59 -0300

    implementing req-reply

---
+++ ZeromqLabs/Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ZeroMQ;
+
+namespace Client
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var endpoint = "tcp://191.232.181.23:5560";
+            
+            using (var context = new ZContext())
+            using (var socket = new ZSocket(context, ZSocketType.REQ))
+            {
+                Console.WriteLine($"Conectando-se ao endpoint {endpoint}");
+                
+                socket.Connect(endpoint);
+                var count = 1;
+                
+                while (count < 10001)
+                {
+                    var frame = new ZFrame(count.ToString());
+                    socket.Send(frame);
+                    
+                    var reply = socket.ReceiveFrame();
+                    Console.WriteLine($"Resposta {reply.ToString()} recebida");
+                    
+                    count++;
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ZeromqLabs/Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ZeroMQ;
+
+namespace Server
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var endpoint = "tcp://192.168.60.87:5560";
+            
+            using (var context = new ZContext())
+            using (var socket = new ZSocket(context, ZSocketType.REP))
+            {
+                Console.WriteLine($"Criando endpoint {endpoint}");
+                socket.Connect(endpoint);
+                Console.WriteLine("Aguardando mensagens");
+                
+                while (true)
+                {
+                    var message = socket.ReceiveFrame();
+                    Console.WriteLine("Processando mensagem: " + message.ToString());
+                    var reply = new ZFrame("resposta:" + message.ToString());
+                    
+                    socket.Send(reply);
+                }
+                
+                Console.ReadLine();
+            }
+        }
+    }
+}
